98cc57d5ee9131145df8c49d5d5a5ac89afebaf9
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-17 17:37:05 +0500

    in attendanceController we create index function to shows values.

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Imports\AttendanceImport;
 use App\Models\Employee;
+use App\Services\AttendanceService;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
 class AttendanceController extends Controller
 {
+    public function index()
+    {
+        $attendanceService = new AttendanceService();
+        $attendanceInformation = $attendanceService->attendanceWithWorkingHours();
+        dd($attendanceInformation);
+
+        return view('attendance.index', ['attendanceInformation' => $attendanceInformation]);
+    }
+
     public function uploadAttendance(Request $request)
     {
         $this->validate($request, [
@@ -17,28 +27,6 @@ class AttendanceController extends Controller
 
         $file = $request->file('file');
         Excel::import(new AttendanceImport, $file->store('files'));
-
-        // if (count(AttendanceImport::$failures) > 0) {
-        // Handle failures
-        // return response()->json(['message' => 'Failed to import data.'], 400);
-        // }
-
         return response()->json(['message' => 'Data imported successfully.'], 200);
-    }
-
-    public function getEmployeeAttendance($id)
-    {
-        $employee = Employee::find($id);
-
-        if (!$employee) {
-            return response()->json(['error' => 'Employee not found'], 404);
-        }
-        $attendanceRecords = $employee->attendance;
-        $totalWorkingHours = $attendanceRecords->sum('working_hours');
-        return response()->json([
-            'employee' => $employee,
-            'attendance' => $attendanceRecords,
-            'total_working_hours' => $totalWorkingHours,
-        ], 200);
     }
 }
