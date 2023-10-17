c4d0649765245cc777dd98469efb2f23d8395c41
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-17 17:34:05 +0500

    create 'attendance' function to get values through foreign keys

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    public function attendance()
+    {
+        return $this->belongsTo(Attendance::class, 'attendance_id', 'id');
+    }
 }
