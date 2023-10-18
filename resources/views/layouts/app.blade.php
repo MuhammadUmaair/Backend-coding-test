8633bb6e9a60e44449fb199bfe413a85e08463ea
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-18 19:50:55 +0500

    create app layout file, this file use as a master file for blade template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Find Duplicates</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="{{ ('css/style.css') }}">
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
