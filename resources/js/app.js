d7f70377b480aa68b5a7bd5a78a713acfe86afc8
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-18 18:47:33 +0500

    add css and js file for react list page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import AttendanceList from './components/AttendanceList';
+
+if (document.getElementById('app')) {
+    const root = createRoot(document.getElementById('app'));
+    root.render(<AttendanceList />);
+}
