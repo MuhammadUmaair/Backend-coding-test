faa37af762d4d5d237a6365efa7b3e76f2cf07c7
Muhammad Umair <mohammad.umair9610@gmail.com>
2023-10-18 19:49:58 +0500

    create two routes for get and post array values

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArrayController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(ArrayController::class)->group(function () {
+    Route::get('/get-input',  'index')->name('arrayinput');
+    Route::post('/find-duplicates', 'findDuplicates')->name('findDuplicates');
 });
